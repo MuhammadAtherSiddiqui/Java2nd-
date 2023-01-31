a3ad9dbcf74df99ed9946299abdc972e30123318
MuhammadAtherSiddiqui <122442314+MuhammadAtherSiddiqui@users.noreply.github.com>
2023-01-31 04:18:45 -0800

    Add a new file index and app

---
+++ App.js
@@ -0,0 +1,123 @@
+// ---------------CHAPTER = 02
+//Q NO # 01
+
+//var username;
+
+// Q NO # 02
+
+//username = "Ather";
+
+//Q NO # 03
+
+//var js;
+
+//js = "Hello World";
+
+//alert(js);    
+
+//Q NO # 04
+
+// var js;
+
+// js = "Ather Siddiqui";
+
+// alert(js);
+
+// js = "20 years";
+
+// alert(js);
+
+// js="Certified Mobile App Development"
+
+// alert(js);
+
+//Q NO # 05
+
+//alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+//Q NO # 06
+
+// var email;
+
+// email = "ather@gmail.com";
+
+// alert(email);
+
+//Q NO # 07
+
+// var book;
+
+// book ="I am trying to learn form the book A smarter way to learn javascript";
+
+// alert(book);
+
+//Q NO # 08
+
+//document.write("Yha! I can write HTML content to javascrpit");
+
+//Q NO # 09
+
+// document.write(" “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”  ");
+
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+// ---------------CHAPTER = 03
+
+//Q NO # 01
+
+// var age;
+
+// age ="20 years old";
+
+// alert("I am "+age);
+
+//Q NO # 02
+
+// var num;
+
+// num ="14";
+
+// alert ("You have visited this site "+num+" Times");
+
+//Q NO # 03
+
+// var birthYear = (2002);
+
+// document.write("My birth year is " +birthYear+"<br /> Data type my decleared variable is Number");
+
+//Q NO # 04
+
+// var name = ("Ather");
+
+// var product = ("T-shirt(s)");
+
+// var qan = (5);
+
+// document.write('“<b>'+name+'</b> ordered <b>'+qan+ product+'</b> on XYZ Clothing store”.');
+
+
+
+
+// ---------------CHAPTER = 04
+
+//Q NO # 01
+
+//var name , hight , age;
+
+//Q NO # 02
+
+// Legal Var
+
+// var name , hight , age , email , $my;
+
+// ill-legal Var
+
+// var fnaame , ------password ,
+
+//Q NO # 03
+
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("Variable names can only contain letter, numbers, $ and _.For example $my_1stVariable");
+// document.write("<br />Variables must begin with a letter, $ or _. For example $name, _name or name");
+// document.write("<br /> Variable names are case sensitive.");
+// document.write("<br /> Variable names should not be JS keywords.");
